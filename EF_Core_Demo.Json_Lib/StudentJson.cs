d04d597e30418024ad3c61e07bc2ac1d828eccff
Starinin Andrey <starininandrey@gmail.com>
2022-11-07 16:24:24 +0300

    Add StudentJson

---
+++ EF_Core_Demo.Json_Lib/StudentJson.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using EF_Core_Demo.Models;
+
+namespace EF_Core_Demo.Json_Lib;
+
+public static class StudentJson
+{
+    public static void Export(IEnumerable<Student> students, string path = "student.json")
+    {
+        var json = JsonSerializer.Serialize(students);
+        File.WriteAllText(path, json);
+    }
+
+    public static IEnumerable<Student>? Import(string path = "student.json")
+    {
+        var json = File.ReadAllText(path);
+        return JsonSerializer.Deserialize<IEnumerable<Student>>(json);
+    }
+}
