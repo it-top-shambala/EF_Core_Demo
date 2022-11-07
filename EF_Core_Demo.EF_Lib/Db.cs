ba3afcb07d6d528a5487d6ddd93112f4a01d5379
Starinin Andrey <starininandrey@gmail.com>
2022-11-07 16:33:36 +0300

    Add Db

---
+++ EF_Core_Demo.EF_Lib/Db.cs
@@ -0,0 +1,23 @@
+﻿using DbConfigLib;
+using EF_Core_Demo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF_Core_Demo.EF_Lib;
+
+public class Db : DbContext
+{
+    public DbSet<Student> Students { get; set; }
+
+    public Db() { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySQL(DbConfig.ImportFromJson().ToString());
+    }
+
+    public void CreateDb()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+}
