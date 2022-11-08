d4381cdc28e5424b7f21caaba2f66af658b34284
Starinin Andrey <starininandrey@gmail.com>
2022-11-08 17:08:20 +0300

    Update

--- EF_Core_Demo.App/Program.cs
+++ EF_Core_Demo.App/Program.cs
@@ -1,20 +1,20 @@
 ﻿using EF_Core_Demo.EF_Lib;
 using EF_Core_Demo.EF_Lib.Models;
 using EF_Core_Demo.Json_Lib;
-using EF_Core_Demo.Models;
+using Microsoft.EntityFrameworkCore;
 using Student = EF_Core_Demo.EF_Lib.Models.Student;
 
 
 var students = StudentJson.Import();
 
-var db = new Db();
-db.CreateDb();
+var tableStudents = new List<Student>();
+var tableSubjects = new List<Subject>();
+var tableStudentsRatings = new List<StudentsRating>();
 
 foreach (var student in students)
 {
-    db.Students.Add(new Student
+    tableStudents.Add(new Student
     {
-        Id = student.Id,
         FirstName = student.FirstName,
         LastName = student.LastName,
         DateOfBirth = student.DateOfBirth
@@ -22,12 +22,38 @@ foreach (var student in students)
 
     foreach (var (subject, _) in student.Ratings)
     {
-        foreach (var dbSubject in db.Subjects)
+        if (tableSubjects.Count == 0)
         {
-            if (dbSubject.Name == subject ) continue;
+            tableSubjects.Add(new Subject { Name = subject });
+        }
 
-            db.Subjects.Add(new Subject() { Name = subject});
+        var findSubject = tableSubjects.FirstOrDefault(s => s.Name == subject);
+        if (findSubject == null)
+        {
+            tableSubjects.Add(new Subject { Name = subject });
+        }
+    }
+
+    foreach (var (subject, marks) in student.Ratings)
+    {
+        var _student = tableStudents.First(s => s.LastName == student.LastName && s.FirstName == student.FirstName && s.DateOfBirth == student.DateOfBirth);
+        var _subject = tableSubjects.First(s => s.Name == subject);
+
+        foreach (var mark in marks)
+        {
+            tableStudentsRatings.Add(new StudentsRating
+            {
+                Student = _student,
+                Subject = _subject,
+                Mark = mark
+            });
         }
     }
 }
 
+var db = new Db();
+db.CreateDb();
+db.Students.AddRange(tableStudents);
+db.Subjects.AddRange(tableSubjects);
+db.StudentsRatings.AddRange(tableStudentsRatings);
+db.SaveChanges();
